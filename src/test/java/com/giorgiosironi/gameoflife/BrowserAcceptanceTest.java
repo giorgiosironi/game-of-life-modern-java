103bc30a81854e8522db00137694603596465314
Giorgio Sironi <info@giorgiosironi.com>
2015-12-20 19:36:35 +0100

    First failing acceptance test

--- src/test/java/com/giorgiosironi/gameoflife/BrowserAcceptanceTest.java
+++ src/test/java/com/giorgiosironi/gameoflife/BrowserAcceptanceTest.java
@@ -1,22 +1,35 @@
 package com.giorgiosironi.gameoflife;
 
-import org.junit.BeforeClass;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class BrowserAcceptanceTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	private static WebDriver driver;
+
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+	}
+	
+	@After
+	public void tearDown() {
+		driver.quit();
 	}
 
 	@Test
-	public void test() {
-		WebDriver driver = new FirefoxDriver();
-		driver.get("http://www.google.com");
-		driver.quit();
+	public void test() throws InterruptedException {
+		driver.get("http://localhost:8080");
+		WebElement element = driver.findElement(By.cssSelector("h1"));
+		assertEquals("Game Of Life", element.getText());
 	}
 
 }
