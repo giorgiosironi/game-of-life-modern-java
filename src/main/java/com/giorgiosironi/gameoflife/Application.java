f329be9904edcba26a9c715a9c21915fda3e165c
Giorgio Sironi <info@giorgiosironi.com>
2015-12-28 11:09:54 +0100

    Terminating Jetty immediately in case of startup problem

--- src/main/java/com/giorgiosironi/gameoflife/Application.java
+++ src/main/java/com/giorgiosironi/gameoflife/Application.java
@@ -11,40 +11,54 @@ public class Application implements Runnable {
 	private Server server;
 	private boolean started;
 	private Object startedNotification;
+	private static final int PORT = 8080;
 	
 	public Application() {
 		startedNotification = new Object();
 	}
 
 	public void run() {	
-		try {
-			synchronized (startedNotification) {
-				started = false;
-				server = new Server(8080);
-				
-				ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-				context.setContextPath("/");
-				context.addServlet(GameOfLifeServlet.class, "/plane");
-				
-			    ResourceHandler resource_handler = new ResourceHandler();
-			    resource_handler.setDirectoriesListed(true);
-			    // how to point to a dynamic page?
-			    resource_handler.setWelcomeFiles(new String[]{ "index.html" });	
-			    resource_handler.setResourceBase(this.getClass().getResource("/static").toString());
-			    
-			    HandlerList handlers = new HandlerList();
-				handlers.setHandlers(new Handler[] { resource_handler, context });
-			    server.setHandler(handlers);
-			 	server.start();
-				started = true;	
-				startedNotification.notify();
+		synchronized (startedNotification) {
+			started = false;
+			server = new Server(PORT);
+			
+			ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+			context.setContextPath("/");
+			context.addServlet(GameOfLifeServlet.class, "/plane");
+			
+		    ResourceHandler resource_handler = new ResourceHandler();
+		    resource_handler.setDirectoriesListed(true);
+		    // how to point to a dynamic page?
+		    resource_handler.setWelcomeFiles(new String[]{ "index.html" });	
+		    resource_handler.setResourceBase(this.getClass().getResource("/static").toString());
+		    
+		    HandlerList handlers = new HandlerList();
+			handlers.setHandlers(new Handler[] { resource_handler, context });
+		    server.setHandler(handlers);
+		 	try {
+				server.start();
+			} catch (Exception e) {
+				try {
+					// TODO: log exception somewhere
+					System.out.println("Shutting down immediately");
+					server.stop();
+					System.out.println("Jetty stopped");
+					return;
+				} catch (Exception e1) {
+					throw new RuntimeException("Cannot shutdown Jetty", e1);
+				}
 			}
-				//server.dumpStdErr();
-			server.join();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			started = true;	
+			startedNotification.notify();
 		}
+			//server.dumpStdErr();
+		try {
+			server.join();
+		} catch (InterruptedException e) {
+			// TODO: log this somewhere
+			System.out.println("Application interrupted");
+		}
+		
 	}
 	
 	public void waitForStartup() throws InterruptedException {
